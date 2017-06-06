755b5aaa67ad7775c7ef316930e4e09c30919e34
TristanElain <tristan.elain@gmail.com>
2017-06-06 20:44:24 +0200

    Premiers layouts

--- app/src/main/java/com/example/e145540w/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/e145540w/projectandroid/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.e145540w.projectandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.nbRésultats);
+
+        List<String> values = new LinkedList<>();
+        values.add("Nombres de valeurs affichées");
+        values.add("5");
+        values.add("10");
+        values.add("15");
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, values);
+
+        spinner.setAdapter(spinnerAdapter);
+
     }
 }
